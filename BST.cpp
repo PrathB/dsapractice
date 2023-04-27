a124de1081506685e96f3b1aed95b280b02e55d3
Pratham Batra <pratham11.batra@gmail.com>
2023-04-27 20:01:36 +0530

    Created new file BST.cpp for binary search tree functions and created function insertBST()

---
+++ BST.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+void inorder(node* root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+node* insertBST(node* root,int val){
+    if(root==NULL){
+        return new node(val);
+    }
+    if((root->data)>val){
+        root->left=insertBST(root->left,val);
+    }
+    else{
+        root->right=insertBST(root->right,val);
+    }
+    return root;
+}
+
+int main(){
+    node* root=NULL;
+    root=insertBST(root,1);
+    insertBST(root,3);
+    insertBST(root,4);
+    insertBST(root,2);
+    insertBST(root,7);
+    inorder(root);
+}
