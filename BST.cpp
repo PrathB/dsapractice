88ce839991426504e22af52bef76dfc9c4b6973a
Pratham Batra <pratham11.batra@gmail.com>
2023-06-09 16:26:52 +0530

    Added function 'deleteInBST' to delete node in BST

--- BST.cpp
+++ BST.cpp
@@ -49,7 +49,39 @@ node* searchBST(node* root, int key){
         return searchBST(root->right,key);
     }
 }
-
+node* inordersucc(node* root){
+    node* curr=root;
+    while(curr && curr->left!=NULL){
+        curr=curr->left;
+    }
+    return curr;
+}
+node* deleteInBST(node* root,int key){
+    if((root->data)>key){
+        root->left=deleteInBST(root->left,key);
+    }
+    else if((root->data)<key){
+        root->right=deleteInBST(root->right,key);
+    }
+    else if((root->data)==key){
+        if(root->left==NULL){
+            node* temp=root->right;
+            free(root);
+            return temp;
+        }
+        else if(root->right==NULL){
+            node* temp=root->left;
+            free(root);
+            return temp;
+        }
+        else if(root->left!=NULL && root->right!=NULL){
+            node* temp=inordersucc(root->right);
+            root->data=temp->data;
+            root->right=deleteInBST(root->right,temp->data);
+        }
+    }
+    return root;
+}
 int main(){
     node* root=NULL;
     root=insertBST(root,1);
@@ -57,5 +89,6 @@ int main(){
     insertBST(root,4);
     insertBST(root,2);
     insertBST(root,7);
+    root=deleteInBST(root,3);
     inorder(root);
 }
