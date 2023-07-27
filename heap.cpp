9a6665de6dd51ff51cd71c5a50298ef54717c5bd
Pratham Batra <pratham11.batra@gmail.com>
2023-07-27 20:35:25 +0530

    Added function deleteroot() in heap

--- heap.cpp
+++ heap.cpp
@@ -19,10 +19,29 @@ class heap{
         while(index>1){
             int parent=index/2;
             if(arr[parent]<arr[index]){
-                int temp=arr[parent];
-                arr[parent]=arr[index];
-                arr[index]=temp;
+                swap(arr[parent],arr[index]);
                 index=parent;
+            }
+            else{
+                return;
+            }
+        }
+    }
+
+    void deleteroot(){
+        arr[1]=arr[size];
+        size--;
+        int i=1;
+        while(i<size){
+            int lchild=2*i;
+            int rchild=2*i+1;
+            if(lchild<=size && arr[i]<arr[lchild] && arr[lchild]>arr[rchild]){
+                swap(arr[lchild],arr[i]);
+                i=lchild;
+            }
+            else if(rchild<=size && arr[i]<arr[rchild] && arr[rchild]>arr[lchild]){
+                swap(arr[rchild],arr[i]);
+                i=rchild;
             }
             else{
                 return;
@@ -45,5 +64,8 @@ int main(){
     h.insert(53);
     h.insert(52);
     h.insert(54);
+    h.print();
+    cout<<endl;
+    h.deleteroot();
     h.print();
 }
