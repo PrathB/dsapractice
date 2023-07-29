ae45537e49866f38fd33cf2141e5a9772f8d50e8
Pratham Batra <pratham11.batra@gmail.com>
2023-07-29 16:23:42 +0530

    Added functions to check if a binary tree is heap

--- heap.cpp
+++ heap.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
 
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+
 class heap{
     public:
     int arr[100];
@@ -86,6 +99,57 @@ void heapsort(int arr[],int n){
     }
     return;
 }
+
+//Is binary tree a heap
+//for 0 indexing
+
+int totalnodes(node* root){
+    if(root==NULL){
+        return 0;
+    }
+
+    int lnodes=totalnodes(root->left);
+    int rnodes=totalnodes(root->right);
+    return(1+lnodes+rnodes);
+}
+
+bool isCBT(node* root,int i,int n){
+    if(root==NULL){
+        return true;
+    }
+    if(root!=NULL && i>=n){
+        return false;
+    }
+    bool left=isCBT(root->left,2*i+1,n);
+    bool right=isCBT(root->right,2*i+2,n);
+    return(left && right);
+}
+
+bool heapproperty(node* root){
+    if(root==NULL){
+        return true;
+    }
+    if(root->left==NULL && root->right==NULL){
+        return true;
+    }
+    if(root->right==NULL){
+        return(root->data>root->left->data);
+    }
+    bool left=heapproperty(root->left);
+    bool right=heapproperty(root->right);
+    if(left && right && root->data >root->left->data && root->data >root->right->data){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+bool isheap(node* root){
+    int n=totalnodes(root);
+    if(isCBT(root,0,n) && heapproperty(root)){
+        return true;
+    }
+}
 int main(){
     heap h;
     h.insert(50);
@@ -119,5 +183,12 @@ int main(){
     for(int i=1;i<=n;i++){
         cout<<arr[i]<<" ";
     }
+    cout<<endl;
+
+    //isheap
+    node* root=new node(60);
+    root->left=new node(30);
+    root->right=new node(50);
+    cout<<isheap(root);
 
 }
